d8a902fc9cd46ae6c741f9b4fa6a4025e32fb811
Josh Mann <jlmann72@gmail.com>
2020-08-11 15:21:19 -0400

    Explorer Mode Completed

--- Program.cs
+++ Program.cs
@@ -2,11 +2,54 @@
 
 namespace VariablesCS
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            // Declare Variables
+            var numberOfCupsOfCoffee = 3;
+            var fullName = "Josh Mann";
+            var today = "08/11/2020";
+
+            // Output to Terminal
+            Console.WriteLine($"numberOfCupsOfCoffee: {numberOfCupsOfCoffee} fullName: {fullName} today: {today}");
+
+            // Get Input from User
+            Console.Write("What is your name? ");
+            var userName = Console.ReadLine();
+            Console.WriteLine($"Hello, {userName}.");
+
+            // Getting different types of input from the user
+            Console.WriteLine("Please enter 2 numbers.");
+            Console.Write("First Number: ");
+            string firstNumberAsString = Console.ReadLine();
+            Console.Write("Second Number: ");
+            string secondNumberAsString = Console.ReadLine();
+
+
+            // Converting string input into numbers
+            var firstOperand = double.Parse(firstNumberAsString);
+            var secondOperand = double.Parse(secondNumberAsString);
+
+
+            // Doing Math
+
+            // sum
+            var sum = firstOperand + secondOperand;
+            Console.WriteLine($"sum = {sum}");
+            // difference
+            var difference = firstOperand - secondOperand;
+            Console.WriteLine($"difference = {difference}");
+            // product
+            var product = firstOperand * secondOperand;
+            Console.WriteLine($"product = {product}");
+            // quotient
+            var quotient = firstOperand / secondOperand;
+            Console.WriteLine($"quotient = {quotient}");
+            // remainder (modulo)
+            var remainder = firstOperand % secondOperand;
+            Console.WriteLine($"remainder = {remainder}");
+            // display
+        }
     }
-  }
 }
